366b689f68877d0e37b105baba1cb3a962b11f42
wmalfian <156230263+wmalfian@users.noreply.github.com>
2025-06-12 09:10:41 +0800

    Update RequestCancelBookingServlet.java

--- src/java/controller/RequestCancelBookingServlet.java
+++ src/java/controller/RequestCancelBookingServlet.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 import dao.BookingDAO;
 import model.Booking;
 import util.DBConnection;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
