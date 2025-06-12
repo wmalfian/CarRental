6308c5cbe0e1cd87e42e7f670ac24f42fd066a93
wmalfian <156230263+wmalfian@users.noreply.github.com>
2025-06-12 09:03:46 +0800

    Update DeleteUserServlet.java

--- src/java/controller/DeleteUserServlet.java
+++ src/java/controller/DeleteUserServlet.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 import dao.UserDAO;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
