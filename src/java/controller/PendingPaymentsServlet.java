5291ee19d95c6ce34f9bc34a22ea66b869ca4213
wmalfian <156230263+wmalfian@users.noreply.github.com>
2025-06-12 09:09:52 +0800

    Update PendingPaymentsServlet.java

--- src/java/controller/PendingPaymentsServlet.java
+++ src/java/controller/PendingPaymentsServlet.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 import dao.BookingDAO;
 import dao.CarDAO;
 import model.Booking;
 import model.Car;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
