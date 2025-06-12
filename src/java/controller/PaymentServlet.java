b1412d100a1db2ef5d033e70137e01e2134f566b
wmalfian <156230263+wmalfian@users.noreply.github.com>
2025-06-12 09:09:37 +0800

    Update PaymentServlet.java

--- src/java/controller/PaymentServlet.java
+++ src/java/controller/PaymentServlet.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 import dao.PaymentDAO;
 import dao.BookingDAO; 
 import model.Payment;
 import util.DBConnection;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
